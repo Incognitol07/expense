50b881859de8281a4c52888aa9676f6c5445e363
AdelodunAbraham <ab.adelodun@gmail.com>
2024-10-14 00:35:04 +0100

    Working principle of class Record

--- main.py
+++ main.py
@@ -14,5 +14,27 @@ class Edit:
     ...
 
 class Record:
-    ...
+    def __init__(self, date, description, amount, category):
+        self._date = date
+        self._description = description
+        self._amount = amount
+        self._category = category
 
+    @property
+    def date(self):
+        return self._date
+    
+    @property
+    def description(self):
+        return self._description
+    
+    @property
+    def amount(self):
+        return self._amount
+    
+    @property
+    def category(self):
+        return self._category
+    
+    def details(self):
+        return {"date": self.date, "description": self.description, "amount": self.amount, "category": self.category}
